3997b27ccf2ae040c064f5c52eeff7d253fb4aea
TuanNghia <tuannghiait2905@gmail.com>
2026-01-14 00:25:53 +0700

    Update Endpoints

--- src/apis/endpoints.ts
+++ src/apis/endpoints.ts
@@ -1,5 +1,5 @@
-export const END_POINTS = 'http://127.0.0.1:3000/api/v1/';
-// export const END_POINTS = 'https://king-introduces-furnishings-affected.trycloudflare.com/api/v1/';
+// export const END_POINTS = 'http://127.0.0.1:3000/api/v1/';
+export const END_POINTS = 'https://king-introduces-furnishings-affected.trycloudflare.com/api/v1/';
 export const ACCESS_TOKEN_KEY = 'access_token';
 export const REFRESH_TOKEN_KEY = 'refresh_token';
 export const USER_KEY = 'user';
